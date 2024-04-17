877f5b19f9eac1901aaff29e00cb891ea4e5a872
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2024-04-17 15:35:20 +0000

    commande

---
+++ src/Form/CommandeType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commande;
+use App\Entity\Fournisseur;
+use App\Repository\FournisseurRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommandeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+//            ->add('date', null, [
+//                'widget' => 'single_text',
+//            ])
+            ->add('Fournisseur', EntityType::class, [
+                'class' => Fournisseur::class,
+                'choice_label' => function($fournisseur){
+                    return $fournisseur->getNom();
+                },
+                'multiple' => false,
+                'placeholder' => 'Sélectionnez un fournisseur *',
+                'label' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commande::class,
+        ]);
+    }
+}
